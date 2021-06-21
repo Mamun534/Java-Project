74e1a8b5068f4f5d54e8c78446a2f976226f9b17
mamun534 <mamunqa.test@gmail.com>
2021-06-21 19:21:27 -0400

    second commit

---
+++ Work Speace2/MavenProject/src/main/java/com/my/MavenProject/App.java
@@ -0,0 +1,14 @@
+package com.my.MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Mamun Chowdhury....!");
+    }
+}
